a3c87cbf96489b70a0ed6ab3576f1749ca4ccb2c
Sergei Vdovenkov <vdovenkov2000@gmail.com>
2024-06-06 01:04:33 +0300

    Fix errors

--- src/app.py
+++ src/app.py
@@ -5,12 +5,11 @@ def main():
     try:
         x = float(input("Enter 1-st number: "))
         y = float(input("Enter 2-nd number: "))
-        operator = input("Enter operation: ")
-        print(f"Result: {x} {operator} {y} = {calculate(x, y, operator)}")
+        op = input("Enter operation: ")
+        print(f"Result: {x} {op} {y} = {calculate(x, y, op)}")
     except ValueError as err:
-        print(f'Error: {err}')
+        print(f"Error: {err}")
     except KeyError:
-        print(f"Error: unknown operator '{operator}'. Cannot calculate expression.")
+        print(f"Error: unknown operator '{op}'. Cannot calculate expression.")
     except ZeroDivisionError:
         print("Error: cannot divide, second operand equal 0.")
-
