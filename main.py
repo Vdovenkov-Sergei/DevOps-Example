00b87604ef0b9edabcaa3fee40eec2da1fd8b195
Sergei Vdovenkov <vdovenkov2000@gmail.com>
2024-06-05 23:46:39 +0300

    Review code

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
-def add(x, y):
-    return x + y
-
-
-def sub(x, y):
-    return x + y
-
-
-def mult(x, y):
-    return x * y
-
-
-def delete(x, y):
-    return x / y
+func = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "*": lambda x, y: x * y,
+    "/": lambda x, y: x / y,
+}
 
 
 def calculate(x, y, operator):
-    if operator == "+":
-        return add(x, y)
-    elif operator == "-":
-        return sub(x, y)
-    elif operator == "*":
-        return mult(x, y)
-    elif operator == "/":
-        return delete(x, y)
+    return func[operator](x, y)
 
 
-x = int(input("Enter 1-st number: "))
-y = int(input("Enter 2-nd number: "))
-operator = input("Enter operation: ")
-print(f"Result: {calculate(x, y, operator)}")
+def main():
+    x = int(input("Enter 1-st number: "))
+    y = int(input("Enter 2-nd number: "))
+    operator = input("Enter operation: ")
+    print(f"Result: {x} {operator} {y} = {calculate(x, y, operator)}")
+
+
+if __name__ == "__main__":
+    main()
