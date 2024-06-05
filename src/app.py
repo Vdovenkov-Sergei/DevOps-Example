f0af9a96ee757ac2a96ebc09e0b8997db116b632
Sergei Vdovenkov <vdovenkov2000@gmail.com>
2024-06-06 01:24:30 +0300

    Add cyclic interaction

--- src/app.py
+++ src/app.py
@@ -4,14 +4,24 @@ from src.greeting import greeting
 
 def main():
     greeting(input("What's your name? "))
-    try:
-        x = float(input("Enter 1-st number: "))
-        y = float(input("Enter 2-nd number: "))
-        operator = input("Enter operation: ")
-        print(f"Result: {x} {operator} {y} = {calculate(x, y, operator)}")
-    except ValueError as err:
-        print(f"Error: {err}")
-    except KeyError:
-        print(f"Error: unknown operator '{operator}'. Cannot calculate expression.")
-    except ZeroDivisionError:
-        print("Error: cannot divide, second operand equal 0.")
+
+    while True:
+        try:
+            x = float(input("Enter 1-st number: "))
+            y = float(input("Enter 2-nd number: "))
+            op = input("Enter operation: ")
+            print(f"Result: {x} {op} {y} = {calculate(x, y, op)}\n")
+        except ValueError as err:
+            print(f"Error: {err}\n")
+        except KeyError:
+            print(f"Error: can't calculate, unknown operator '{op}'\n")
+        except ZeroDivisionError:
+            print("Error: can't divide, second operand equal 0.\n")
+
+        answer = input("More? ").lower()
+        if answer == "yes":
+            continue
+        elif answer.lower() == "no":
+            break
+        else:
+            print("I didn't understand you, but give me another example.")
